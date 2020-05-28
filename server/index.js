a0c0470e7270748a773684b38f81572bd0d8d65c
Tim <meiertim745@gmail.com>
2020-05-28 15:58:47 +0200

    grpc klappt und proto änderungen

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require('body-parser');
 const path = require('path');
-const Mali = require('mali');
+const mali = require('mali');
 
 var index = require("./routes/index");
 var patientenakte = require("./routes/patientenakte");
@@ -30,3 +30,46 @@ app.listen(port, function(){
     console.log(`Example app listening at http://localhost:${port}`);
 });
 
+
+/* grpc Server */
+const gRpcServer = new mali();
+const krankenakteProtoPath = path.resolve(__dirname, 'proto/krankenakte.proto');
+const ueberweisungProtoPath = path.resolve(__dirname, 'proto/ueberweisung.proto');
+gRpcServer.addService(krankenakteProtoPath, 'KrankenakteService');
+gRpcServer.addService(ueberweisungProtoPath, 'UeberweisungService');
+
+function sendUeberweisung (param){
+    param.res={
+        "status":"200",
+        "message":"Hat geklappt"
+    }
+}
+
+function getKrankenakte (param){
+    param.res={
+        "userid": "700",
+        "patientenakte": [{
+            "patientenakteid": "hans",
+            "userid": "hans",
+            "datum": "hans",
+            "anamnese": "hans",
+            "symptome": "hans",
+            "diagnose": "hans",
+            "medikation": "hans",
+            "psychischkrank": "hans",
+            "sonstiges": "hans"
+        }]
+    }
+}
+
+function updatePatientenakte (param){
+    param.res={
+        "status":"200",
+        "message":"hat geklappt"
+    }
+}
+
+/*Launch gRPC server*/
+gRpcServer.use({sendUeberweisung, updatePatientenakte, getKrankenakte});
+gRpcServer.start("0.0.0.0:50051");
+console.log("gRPC Server running on port: 50051");
