bc3c37a814eae20155e0d9d67f05d3421e854263
TSMeier <32977524+TSMeier@users.noreply.github.com>
2020-06-07 16:08:57 +0200

    Pfade für deploy angepasst

---
+++ frontend/src/scripts/paths.js
@@ -0,0 +1,4 @@
+const url_krankheit = "http://hausarzt.dvess.network/api/krankheitsstatistik"
+const url_patienten = "http://hausarzt.dvess.network/api/patienten"
+const url_patientenakte = "http://hausarzt.dvess.network/api/patientenakte"
+const url_base = "http://hausarzt.dvess.network/api"
