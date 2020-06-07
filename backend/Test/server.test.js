a82f45b03851c8d9d0f781040443458e1a4bd0b8
Tim <meiertim745@gmail.com>
2020-06-07 17:54:52 +0200

    tests angepasst

--- backend/Test/server.test.js
+++ backend/Test/server.test.js
@@ -2,10 +2,30 @@
 const app = require('../index');
 const request = require('supertest');
 
-it('gets the test endpoint', async done => {
+it('check if test responds', async done => {
     const response = await request(app).get('/test')
     
     expect(response.status).toBe(200)
-    expect(response.text).toBe("test message")
+    done()
+});
+
+it('check if krankheitsstatistik responds', async done => {
+    const response = await request(app).get('/api/krankheitsstatistik')
+    
+    expect(response.status).toBe(200)
+    done()
+});
+
+it('check if patienten/all responds', async done => {
+    const response = await request(app).get('/api/patienten/all')
+    
+    expect(response.status).toBe(200)
+    done()
+});
+
+it('check if patientenakte with id 3 responds', async done => {
+    const response = await request(app).get('/api/patientenakte/3')
+    
+    expect(response.status).toBe(200)
     done()
 });
