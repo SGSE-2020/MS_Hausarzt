0542fd18222fafd89672c87a78ccd7197386ce17
Tim <meiertim745@gmail.com>
2020-06-07 17:12:47 +0200

    update routes for deply

--- backend/routes/krankheitsstatistik.js
+++ backend/routes/krankheitsstatistik.js
@@ -22,9 +22,9 @@ krankheitsstatistik_dummy = {
 //});
 router.get('/krankheitsstatistik', function (req, res) {
     response = krankheitsstatistik_dummy;
-    res.set("Access-Control-Allow-Origin", "*");
-    res.set("Access-Control-Allow-Methods", "GET, POST, OPTIONS"); 
-    res.set('Access-Control-Allow-Credentials', true);
+    //res.set("Access-Control-Allow-Origin", "*");
+    //res.set("Access-Control-Allow-Methods", "GET, POST, OPTIONS"); 
+    //res.set('Access-Control-Allow-Credentials', true);
     res.set('Content-Type', 'application/json');
     res.status(200);
     res.json(response);
--- backend/routes/patientenakte.js
+++ backend/routes/patientenakte.js
@@ -57,9 +57,9 @@ patientenakte_dummy = [{
 
 router.get('/patientenakte/:id', function (req, res) {
     response = patientenakte_dummy[req.params.id-1];
-    res.set("Access-Control-Allow-Origin", "*");
-    res.set("Access-Control-Allow-Methods", "GET, POST, OPTIONS"); 
-    res.set('Access-Control-Allow-Credentials', true);
+    //res.set("Access-Control-Allow-Origin", "*");
+    //res.set("Access-Control-Allow-Methods", "GET, POST, OPTIONS"); 
+    //res.set('Access-Control-Allow-Credentials', true);
     res.set('Content-Type', 'application/json');
     res.status(200);
     res.json(response);
